422658bcabbb4bc847ba0ac449041c4cc825f76d
Sean Du <sean.du@bhnetwork.com>
2016-03-01 11:18:38 -0800

    update docker ip

--- script/populate.sh
+++ script/populate.sh
@@ -1,8 +1,8 @@
 curl -v -H "Content-Type: application/json" -X POST -d \
 '{"transactionId":"1234567", "cardNo":"1234567890","channelId":"1000", "upc":"upc3", "loadValue":"110.00", "cardStatus":"ACT", "remark":"comments" }' \
-http://localhost:8080/transaction
+http://192.168.99.100:8080/transaction
 
 
-curl -v -H "Content-Type: application/json" -X GET http://localhost:8080/transaction?id=1234567
+curl -v -H "Content-Type: application/json" -X GET http://192.168.99.100:8080/transaction?id=1234567
 
-curl -v -H "Content-Type: application/json" -X DELETE http://localhost:8080/transaction?id=1234567
+curl -v -H "Content-Type: application/json" -X DELETE http://192.168.99.100:8080/transaction?id=1234567
